48ba723f1f0c3119212ee4446536dd6f3c35f172
MariyaPro <mariaprokof@gmail.com>
2024-03-21 10:32:31 +0300

    replaced RestTemplate with WebClient

--- src/main/java/com/prokofeva/example/payoutcalculator/domain/RequestDto.java
+++ src/main/java/com/prokofeva/example/payoutcalculator/domain/RequestDto.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class RequestDto {
-
     @Digits(integer = 30, fraction = 2)
     @PositiveOrZero
     private Double avgSalary;
--- src/main/java/com/prokofeva/example/payoutcalculator/service/impl/ProductionCalendarServiceImpl.java
+++ src/main/java/com/prokofeva/example/payoutcalculator/service/impl/ProductionCalendarServiceImpl.java
@@ -3,17 +3,18 @@ package com.prokofeva.example.payoutcalculator.service.impl;
 import com.prokofeva.example.payoutcalculator.domain.ResponseProductionCalendarDto;
 import com.prokofeva.example.payoutcalculator.exeption.DateProductionCalendarSourceException;
 import com.prokofeva.example.payoutcalculator.service.ProductionCalendarService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ProductionCalendarServiceImpl implements ProductionCalendarService {
 
     @Value("${production_calendar.source}")
@@ -23,24 +24,31 @@ public class ProductionCalendarServiceImpl implements ProductionCalendarService 
     @Value("${production_calendar.format_response}")
     private String formatResponse;
 
-    private final RestTemplate restTemplate = new RestTemplateBuilder().build();
-
     @Override
     public Integer getAmountOfHolidays(LocalDate firstDay, Integer amountOfDays) {
-        String url = buildUrl(firstDay, amountOfDays);
+        String period = buildPeriod(firstDay, amountOfDays);
 
-        ResponseEntity<ResponseProductionCalendarDto> responseEntity = restTemplate.getForEntity(url, ResponseProductionCalendarDto.class);
-        Optional<ResponseProductionCalendarDto> optionalDto = Optional.ofNullable(responseEntity.getBody());
+        WebClient webClient = WebClient.builder().baseUrl(sourcePath).build();
 
-        return Integer.parseInt(optionalDto.orElseThrow(DateProductionCalendarSourceException::new).getStatistic().get("holidays"));
+        ResponseProductionCalendarDto productionCalendarDtoMono = webClient.get()
+                .uri(String.join("/", country, period, formatResponse))
+                .retrieve()
+                .bodyToMono(ResponseProductionCalendarDto.class)
+                .onErrorResume(e -> Mono.error(DateProductionCalendarSourceException::new))
+                .block();
+
+        return Integer.parseInt(Optional
+                .ofNullable(productionCalendarDtoMono)
+                .orElseThrow(DateProductionCalendarSourceException::new)
+                .getStatistic().get("holidays"));
     }
 
-    private String buildUrl(LocalDate firstDay, Integer amountOfDays) {
+    private String buildPeriod(LocalDate firstDay, Integer amountOfDays) {
         DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
         String startDay = firstDay.format(pattern);
         String finishDay = firstDay.plusDays(amountOfDays - 1).format(pattern);
 
-        return sourcePath + country + '/' + startDay + '-' + finishDay + '/' + formatResponse;
+        return String.join("-", startDay, finishDay);
     }
 }
